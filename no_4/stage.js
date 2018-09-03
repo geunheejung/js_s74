06ebe2cf0e8193fdc222f845c15681c02799052c
geunheejung <gh.jung1@albamapp.com>
2018-09-03 21:29:26 +0900

    Stage 클래스 스피드, 카운트 계산 로직 내부 private 메소드로 분리

--- no_4/stage.js
+++ no_4/stage.js
@@ -3,8 +3,6 @@
  * @type {Stage}
  */
 const Stage = class {
-
-
   // TODO 생성자에서 'listener' 을 받지 않은 이유는
   /*
     Stage가 먼저 만들어져야만 Game 클래스도 만들어질 수 있기에 생성자 시점에서 'listener'을
@@ -20,13 +18,25 @@ const Stage = class {
     this.stage = 0;
     this.next();
   }
+  // 현재 Stage의 게임 진행 속도
+  _speed() {
+    this.speed = 500 - 450 * this.stage / Stage.max;
+  }
+  // 현재 Stage의 목표 블럭 갯수
+  _count() {
+    this.count = 10 + 3 * this.stage;
+  }
   // TODO Stage Class의 역할 중 하나인 현재 스테이지를 다음 스테이지로 넘긴다
   next() {
-    if (this.stage++ < Stage.maxStage) {
-      // speed 값은 ms 단위인데 stage가 올라갈수록 speed는 작아지면서 빨라진다.
-      this.speed = 500 - 450 * this.stage / Stage.maxStage;
-      // stage도 갱신하였고, speed도 갱신하였으니 listener 에게 갱신이 완료되었다고 알린다.
-      this.listener();
+    if (this.stage++ < Stage.max) {
+      // class 를 짤 때 내부의 로직이라 하더라도 관리해야한다 생각하면 메소드로 빼야한다.
+      /*
+        그러한 이유는 메소드로 빼야하는 기존의 로직이 담긴 코드를 다른 코드와 섞여서 감염시키기 싫기 때문이다.
+        메소드로 분리하면 해당 메소드만 관리하면 되기 때문에 메소드로 분리하는것이 상책
+        한번밖에 사용된다 하더라도 관리해야한다 생각한다면 메소드로 분리해야한다.
+       */
+      this._speed();
+      this._count();
     }
   }
   // TODO Stage를 화면에 어떻게 표현할지를 다른 객체가 결정하는것이 아닌 Stage 자기 자신이 결정하게 함
@@ -41,4 +51,4 @@ const Stage = class {
     return `<div>Stage ${this.stage}</div>`;
   }
 }
-Stage.maxStage = 20;
+Stage.max = 20;
